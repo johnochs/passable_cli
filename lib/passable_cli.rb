29c362b00f7fbb89bac3740c276bc04b97b641f0
John Ochs <code@johnochs.io>
2019-09-07 17:34:04 -0700

    Bundler initialization; update gemspec

---
+++ lib/passable_cli.rb
@@ -0,0 +1,6 @@
+require "passable_cli/version"
+
+module PassableCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/passable_cli/version.rb
@@ -0,0 +1,3 @@
+module PassableCli
+  VERSION = "0.1.0"
+end
